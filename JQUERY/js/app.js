1edd6d547f0e9bf7a9f07d62dca0dbdb24ef861d
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-01-12 21:48:03 +0700

    .

--- JQUERY/js/app.js
+++ JQUERY/js/app.js
@@ -1,8 +1,7 @@
-$.get("https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=96381a872b1b405c5bf83b2ed63d9561",
+$.get("https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=9c25d460dde836f770c043f72b304dee",
 function (api) {
     $('.temp').html('Local Temp: ' + api.main.temp + 'C');
     $('.city').html('City : ' + api.name);
     $('.description').html('Description: ' + api.weather[0].description);
-
-    $('.icon img').attr('src','http://openweathermap.org/img/wn' + api.weather[0].icon +'.png');
+  $('.icon img').attr('src','https://openweathermap.org/img/wn' + api.weather[0].icon +'.png');
 });
