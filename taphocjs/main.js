4e4d1315e1ad8ff62dc87c3b6417d756a094d76e
NguyenHuuQuanT2210A <118426160+NguyenHuuQuanT2210A@users.noreply.github.com>
2023-01-12 16:01:02 +0700

    .

---
+++ taphocjs/main.js
@@ -0,0 +1,9 @@
+/*setTimeout(function () {
+alert('Thong bao')
+}, 1000) */
+/*alert('meo')
+setInterval(function () {
+    console.log('day la log' + Mat.random())
+},)*/
+var a = 5;
+var b = 2
---
+++ yTv2.0/js/youtube.js
@@ -0,0 +1,6 @@
+$.get('https://content.googleapis.com/youtube/v3/search?q=" + keyword + "&type=video&maxResults=9&part=snippet&key=AIzaSyDA2efw0ZlxKGyiQvmmaCmfR7MM4rGWlAU',
+function (api) {
+    
+}
+
+)
